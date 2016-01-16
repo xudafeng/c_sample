37f64f36c546171714366afbb44d62c36087decb
xudafeng <xudafeng@126.com>
2016-01-17 02:33:38 +0800

    tweak file

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+# include <stdio.h>
 
 int main(int argc, char *argv[]) {
+
+  int list[] = {1, 2, 3};
+  for (int i = 0; i < sizeof(list); i++) {
+    printf("%d", list[i]);
+  }
   printf("Hello World\n");
   return 0;
 }
