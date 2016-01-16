584aeaf4f546ced10f6be4679f00735e733fd9e2
xudafeng <xudafeng@126.com>
2016-01-17 02:50:44 +0800

    fix

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-
   int list[] = {1, 2, 3};
-  for (int i = 0; i < sizeof(list); i++) {
-    printf("%d", list[i]);
+  for (int i = 0; i < sizeof(list) / sizeof(int); i++) {
+    printf("%d\n", list[i]);
   }
   printf("Hello World\n");
   return 0;
