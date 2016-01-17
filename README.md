af2937ed0544ccf4041291de773ac7525a42c333
xudafeng <xudafeng@126.com>
2016-01-17 18:15:28 +0800

    fix complie

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ```shell
 $ cc -v
-$ cc -c src/main.c && cc -o exec main.o
+$ cc -c src/main.c && cc -o exec main.o && ./exec
 ```
 
 (c) 2014 xdf
--- src/main.c
+++ src/main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+  return a + b;
+}
+
 int main(int argc, char *argv[]) {
   int list[] = {1, 2, 3};
+
   for (int i = 0; i < sizeof(list) / sizeof(int); i++) {
     printf("%d\n", list[i]);
   }
+  int c = add(1, 1);
+
+  printf("sum is %d\n", c);
   printf("Hello World\n");
+
+  struct {
+    int num;
+    char cr[10];
+  } s;
+
+  s.num = 1;
+  printf("struct s num: %d", s.num);
+
   return 0;
 }
